337218b254babf9d8a243fcc6ce549b663dfd0a0
Saba Phruidze <sabaphruidze01@gmail.com>
2024-02-04 14:52:15 +0400

    I currently learned how to add image which fully covers the background , status bar as well. the code is different from expo , because here we need to write translucent aand transparent in statusbar to make image cover it's place as well

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
-import {Text, View, StatusBar, Image} from 'react-native';
+import {Text, View, StatusBar, Image, ImageBackground} from 'react-native';
+
 function App(): React.JSX.Element {
   return (
     <View style={tw`flex-1 relative`}>
-      <StatusBar barStyle="light-content" />
-      <Image
+      <ImageBackground
         source={require('./assets/images/bg.png')}
-        style={tw`absolute h-full w-full`}
-      />
+        style={tw`absolute w-full h-full`}
+        blurRadius={70}>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+      </ImageBackground>
     </View>
   );
 }
