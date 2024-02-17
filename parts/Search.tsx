d16854edb4f5041cf13cb50625403f78c4ae2314
Saba Phruidze <sabaphruidze01@gmail.com>
2024-02-17 16:00:26 +0400

    So I added 2 useState in order to Show search terminal or hide it and show location or hide it, also I added MapPinIcon

--- parts/Search.tsx
+++ parts/Search.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import tw from 'tailwind-react-native-classnames';
-import {View, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 import {theme} from '../theme/Index';
 import {MagnifyingGlassIcon} from 'react-native-heroicons/outline';
+import {MapPinIcon} from 'react-native-heroicons/solid';
 const Search = () => {
+  const [showSearch, toggleSearch] = useState<boolean>(false);
+  const [locations, setLocations] = useState<number[]>([1, 2, 3]);
   return (
     <SafeAreaView style={tw`flex flex-1`}>
       <View style={tw`h-20 mx-4 relative z-50 mt-12`}>
         <View
-          style={tw`flex-row justify-end items-center rounded-full ${theme.bgWhite(
-            20,
-          )} `}>
-          <TextInput
-            style={tw`pl-6 h-10 flex-1 text-base text-white`}
-            placeholder="Search city"
-            placeholderTextColor={'lightgray'}
-          />
+          style={tw`flex-row justify-end items-center rounded-full ${
+            showSearch ? theme.bgWhite(20) : 0
+          } `}>
+          {showSearch ? (
+            <TextInput
+              style={tw`pl-6 pb-2 h-10 flex-1 text-base text-white`}
+              placeholder="Search city"
+              placeholderTextColor={'lightgray'}
+            />
+          ) : null}
           <TouchableOpacity
+            onPress={() => toggleSearch(!showSearch)}
             style={tw`${theme.bgWhite(30)} rounded-full p-3 m-1`}>
             <MagnifyingGlassIcon size={25} color="white" />
           </TouchableOpacity>
         </View>
+        {locations.length > 0 && showSearch ? (
+          <View style={tw`absolute w-full bg-gray-300 top-16 rounded-3xl`}>
+            {locations.map((loc, index) => {
+              return (
+                <TouchableOpacity
+                  key={index}
+                  style={tw`flex-row items-center border-0 p-3 px-4 mb-1 border-b-2 border-b-gray-400`}>
+                  <MapPinIcon size={20} color="gray" />
+                  <Text>London, UNited Kingdom</Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        ) : null}
       </View>
     </SafeAreaView>
   );
